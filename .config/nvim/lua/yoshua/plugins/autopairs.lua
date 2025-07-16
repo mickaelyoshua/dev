31ebae4fd4ddb989b96eb951d78bc7a5cd81bbb9
mickaelyoshua <mickaelyoshua@gmail.com>
2025-07-16 18:23:09 -0300

    new plugins

---
+++ .config/nvim/lua/yoshua/plugins/autopairs.lua
@@ -0,0 +1,8 @@
+-- Autocomplete pairs -> () [] {} "" ''
+return {
+	'windwp/nvim-autopairs',
+	event = "InsertEnter",
+	config = true
+	-- use opts = {} for passing setup options
+	-- this is equivalent to setup({}) function
+}
